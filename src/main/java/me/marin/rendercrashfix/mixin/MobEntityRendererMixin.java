1cadb106b61c9efbe5c4ba8a0416f96837c479b3
marin774 <marinbanozic@gmail.com>
2022-11-22 17:25:20 +0100

    Rework, cancel entity render if world is loading

---
+++ src/main/java/me/marin/rendercrashfix/mixin/MobEntityRendererMixin.java
@@ -0,0 +1,24 @@
+package me.marin.rendercrashfix.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderDispatcher;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EntityRenderDispatcher.class)
+public class MobEntityRendererMixin {
+
+    @Inject(method="render", at = @At("HEAD"), cancellable = true)
+    public <E extends Entity> void renderMixin(E entity, double x, double y, double z, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client.player == null) {
+            ci.cancel();
+        }
+    }
+
+}
